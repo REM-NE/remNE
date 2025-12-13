bf9556d8de242ffb059aa0bc9279d6745e9dbbfc
Samuel.clipes <121581663+samuel123d@users.noreply.github.com>
2025-12-13 14:35:30 -0300

    Footer
    Footer atualizado, porém sem icones interativos.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,45 @@
 import '../App.css';
-
+import logo2 from '../assets/images/logo2.png';
 function Footer() {
     return (
-        <div class="footer">
-            <div class="title-footer">Rodapé</div>
-            <form class="form-footer">
-                <input type="text" defaultValue="Form"/>
-            </form>
+        <div className="footer">
+            <div className="footer-top">
+                <p><a>Mobile app</a></p>
+                <p>Community</p>
+                <p>Company</p>
+
+                <img className="logo" src={logo2} alt="logo" />
+
+                <p>Help desk</p>
+                <p>Blog</p>
+                <p>Resources</p>
+            </div>
+
+            <hr className="footer-hr" />
+
+            <section className="footer-section-bottom">
+                <a href="https://github.com/samuel123d/">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        width="35px"
+                        height="35px"
+                        fill="currentColor"
+                        className="bi bi-github"
+                        viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8..." />
+                    </svg>
+                </a>
+
+                <span>2025 REM - Todos os direitos reservados.</span>
+            </section>
         </div>
+
+
+
+
+
+
     )
+
 }
 
 export default Footer;
