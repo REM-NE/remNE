d0667b4792b1611ff188b673bbb3d34111bd3621
[Denis} <[den_ou@hotmail.com]>
2025-12-17 16:17:07 -0300

    Corrigindo conflitos de estilo

--- src/views/home/page.js
+++ src/views/home/page.js
@@ -112,7 +112,7 @@ export default function Home() {
               <div className="card" onClick={() => { }} style={{ cursor: "pointer" }}>
                 <img src={newsImage1} className="card-img-top" alt="..." />
                 <div className="card-body">
-                  <h5 className="card-title">Lorem Ipsum is simply dummy text of the printing </h5>
+                  <h5 className="card-title home-card-title">Lorem Ipsum is simply dummy text of the printing </h5>
                   {/* <p className="card-text">
                     Some quick example text to build on the card title and make up the bulk of the card’s content.
                   </p> */}
